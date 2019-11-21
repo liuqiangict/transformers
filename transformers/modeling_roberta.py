a58bb0dced78a194fc73a99609b112fde4cea76c
Qiang Liu <qiangliu@microsoft.com>
2019-11-21 21:55:25 +0000

    Update code for random initialization

--- transformers/modeling_roberta.py
+++ transformers/modeling_roberta.py
@@ -320,6 +320,7 @@ class RobertaForSequenceClassification(BertPreTrainedModel):
 
         self.roberta = RobertaModel(config)
         self.classifier = RobertaClassificationHead(config)
+        self.init_weights()
     
     def forward(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None,
                 labels=None):
