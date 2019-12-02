76b853d0554101a7ad0e7a80314b1dd0e0cf5e04
Qiang Liu <qiangliu@microsoft.com>
2019-12-02 03:59:33 +0000

    Add distributed scripts

---
+++ examples/scripts/distributed/node_environment_setup.sh
@@ -0,0 +1,24 @@
+# #!/bin/bash
+# cd /hdfs/bingtee/saksingh
+# sudo apt update
+# sudo apt install vim -y
+# cp .vimrc ~/.vimrc
+# cp -r .vim ~/.vim
+# sudo apt install python3.6 -y
+# sudo apt install python3.6-dev -y
+# sudo -H curl https://bootstrap.pypa.io/ez_setup.py -o - | sudo python3.6 && sudo python3.6 -m easy_install pip
+# pip3.6 install /hdfs/bingtee/samyamr/torch-1.0.1.post2-cp36-cp36m-linux_x86_64.whl
+# pip3.6 install sklearn
+# pip3.6 install imageio
+# pip3.6 install tensorboardX
+# pip3.6 install nltk
+# cd /hdfs/bingtee/saksingh/Bert/pytorch-pretrained-BERT/
+# pip3.6 install -r requirements.txt
+# cp -r /hdfs/bingtee/saksingh/apex/ ~/
+# cd ~/apex/; python3.6 setup.py install --cuda_ext --cpp_ext
+# cd /hdfs/bingtee/saksingh/Bert/pytorch-pretrained-BERT/
+# pip3.6 install --editable .
+# cd /hdfs/bingtee/saksingh/Bert/pytorch-pretrained-BERT/turing/
+
+sudo python3.6 -m pip install regex
+sudo python3.6 -m pip install sacremoses
