38cbcc51f66b62618c626906e82cdd07a685c7ae
Qiang Liu <qiangliu@microsoft.com>
2019-12-02 19:39:35 +0000

    Update code

--- examples/scripts/distributed/app_scripts/multi_target.sh
+++ examples/scripts/distributed/app_scripts/multi_target.sh
@@ -11,29 +11,32 @@ sudo -H pkill python3.6
 export PYTHONPATH=/home/qiangliu/Git/Mine/transformers/
 
 # NCCL_SOCKET_IFNAME=^lo,docker0,veth NCCL_DEBUG=WARN \
-NCCL_IB_DISABLE=1 NCCL_DEBUG=INFO \
+#NCCL_IB_DISABLE=1 NCCL_DEBUG=INFO \
+NCCL_SOCKET_IFNAME=^lo,docker0,veth NCCL_DEBUG=INFO \
+NCCL_TREE_THRESHOLD=0 \
 python3.6 -m torch.distributed.launch \
-    --nproc_per_node=4 \
-    --nnodes=$numnodes \
-    --node_rank=$rank \
-    --master_addr=$masterip \
-    --master_port=2222 \
-    ${APPDIR}/run_glue.py \
-    --input_train_dir /home/qiangliu/Git/Mine/transformers/examples/data/Caption/distill/train/l4_turing_quantus_malta_marco_sbs.tsv \
-    --input_eval_dir /home/qiangliu/Git/Mine/transformers/examples/data/Caption/distill/eval \
-    --model_type roberta \
-    --model_name_or_path /home/qiangliu/Git/Mine/transformers/examples/model/multi_target/3_layers_distill \
-    --task_name qp_multi_target \
-    --do_train \
-    --do_eval \
-    --evaluate_during_training \
-    --max_seq_length 128 \
-    --per_gpu_eval_batch_size=384 \
-    --per_gpu_train_batch_size=384 \
-    --learning_rate 2e-5 \
-    --num_train_epochs 10.0 \
-    --save_steps 2000 \
-    --logging_steps 2000 \
-    --fp16 \
-    --output_dir /home/qiangliu/Git/Mine/transformers/examples/data/Caption/distill/roberta_distill/
+--nproc_per_node=4 \
+--nnodes=$numnodes \
+--node_rank=$rank \
+--master_addr=$masterip \
+--master_port=2222 \
+${APPDIR}/run_glue.py \
+--input_train_dir /home/qiangliu/Git/Mine/transformers/examples/data/Caption/distill/train/l4_turing_quantus_malta_marco_sbs.tsv \
+--input_eval_dir /home/qiangliu/Git/Mine/transformers/examples/data/Caption/distill/eval \
+--model_type roberta \
+--model_name_or_path /home/qiangliu/Git/Mine/transformers/examples/model/multi_target/3_layers_distill \
+--task_name qp_multi_target \
+--do_train \
+--do_eval \
+--evaluate_during_training \
+--max_seq_length 128 \
+--per_gpu_eval_batch_size=384 \
+--per_gpu_train_batch_size=384 \
+--learning_rate 2e-5 \
+--num_train_epochs 10.0 \
+--save_steps 2000 \
+--logging_steps 1000 \
+--fp16 \
+--output_dir /home/qiangliu/Git/Mine/transformers/examples/data/Caption/distill/roberta_distill/
+
 
