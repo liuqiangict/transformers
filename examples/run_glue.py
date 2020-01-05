fe521de82b7ae70c52962f4c506cc52a763c8ad9
Qiang Liu <qiangliu@microsoft.com>
2020-01-05 06:24:18 +0000

    Udpate code

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -731,13 +731,13 @@ def main():
         logger.info("Evaluate the following checkpoints: %s", checkpoints)
         results = {}
         tasks = [
-                    ('qp', 'google', './data/Universial/eval/google/'),
-                    ('qp', 'bing_ann', './data/Universial/eval/bing_ann/'),
-                    ('qp', 'uhrs', './data/Universial/eval/uhrs/'),
-                    ('qp', 'de_de', './data/Universial/eval/de_de/'),
-                    ('qp', 'fr_fr', './data/Universial/eval/fr_fr/'),
-                    #('qp', 'panelone_5k', './data/eval/panelone_5k/'),
-                    #('qp', 'adverserial', './data/eval/adverserial/'),
+                    #('qp', 'google', './data/Universial/eval/google/'),
+                    #('qp', 'bing_ann', './data/Universial/eval/bing_ann/'),
+                    #('qp', 'uhrs', './data/Universial/eval/uhrs/'),
+                    #('qp', 'de_de', './data/Universial/eval/de_de/'),
+                    #('qp', 'fr_fr', './data/Universial/eval/fr_fr/'),
+                    ('qp', 'panelone_5k', './data/eval/panelone_5k/'),
+                    ('qp', 'adverserial', './data/eval/adverserial/'),
                     #('qp', './data/eval/speller_checked/'),
                     #('qp', './data/eval/speller_usertyped/')
                 ]
