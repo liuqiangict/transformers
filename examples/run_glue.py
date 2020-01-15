61204ae1cfadb7bb7ad7228222c3e32e23c124e8
Qiang Liu <qiangliu@microsoft.com>
2020-01-15 02:46:14 +0000

    Udpate code

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -730,7 +730,9 @@ def main():
                     #('qp', './data/eval/speller_usertyped/')
                     #('qp', 'sbs_train', './data/Caption/distill/sbs_data/source_data/train/'),
                     #('qp', 'sbs_eval', './data/Caption/distill/sbs_data/source_data/eval/'),
-                    ('qp', 'sbs_train_' + args.predict_number, './data/Caption/distill/quantus_data/source_data/train_4/' + args.predict_number),
+                    #('qp', 'sbs_train_' + args.predict_number, './data/Caption/distill/quantus_data/source_data/train_4/' + args.predict_number),
+                    ('qp', 'sbs_train_' + args.predict_number, './data/Caption/distill/malta_data/source_data/train_4/' + args.predict_number),
+                    #('qp', 'sbs_train_' + args.predict_number, './data/Caption/distill/malta_data/marco_data/train_4/' + args.predict_number),
                     #('qp', 'sbs_eval', './data/Caption/distill/sbs_data/source_data/eval/'),
 
                 ]
