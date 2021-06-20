785f320caf3710c0d9455791ed9294f94b46e518
Qiang Liu <qiangliu@microsoft.com>
2021-06-20 23:16:25 +0000

    Udpate code

--- src/transformers/trainer_utils.py
+++ src/transformers/trainer_utils.py
@@ -74,7 +74,6 @@ class EvalPrediction(NamedTuple):
 
     predictions: Union[np.ndarray, Tuple[np.ndarray]]
     label_ids: np.ndarray
-    metric_key_prefix: str
 
 
 class PredictionOutput(NamedTuple):
